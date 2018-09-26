63064126e153bea7ef72391fed7cc6dfd5c96fef
Vlad <vladification20@gmail.com>
2018-09-26 10:53:46 +0300

    added all class items pick in level

--- src/Levels/PrologueLevel.cs
+++ src/Levels/PrologueLevel.cs
@@ -66,7 +66,70 @@ namespace WhiteWizard{
 
                 ItemsPick(player,"map");
                 
+            }
+            if(player.GetClass() == "hunter")
+            {   
+                Reset();
+                Console.Write("Pick up the ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("bow&arrow");
+                Reset();
+                Console.Write("?");
+                
+                ItemsPick(player,"bow&arrow");
+
+                Console.Write("Pick up the bottle of ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("water");
+                Reset();
+                Console.Write("?");
+
+                ItemsPick(player, "water");
+
+                Console.Write("Pick up the bag of");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("coins");
+                Reset();
+                Console.Write("?");
+
+                ItemsPick(player,"coins");
             }       
+            if(player.GetClass() == "mage"){
+                
+                Reset();
+                Console.Write("Pick up your ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("staff");
+                Reset();
+                Console.Write("?");
+
+                ItemsPick(player,"staff");
+                
+                Console.Write("Pick up the ");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("book of spells");
+                Reset();
+                Console.Write("?");
+
+                ItemsPick(player,"book of spells");
+                
+                Console.Write("Pick up the ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("mana potions");
+                Reset();
+                Console.Write("?");
+
+                ItemsPick(player,"mana potions");
+                
+                Console.Write("Pick up the bag of");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("coins");
+                Reset();
+                Console.Write("?");
+
+                ItemsPick(player,"coins");
+              
+            }
         }
         #endregion
 
